f0f119c011469d5fbc609918f6456c7249f29775
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-09-10 11:31:09 -0400

    mvp complete

---
+++ src/components/WeatherForcast.jsx
@@ -0,0 +1,19 @@
+import "/Users/andrewsantanastaso/code/ga/labs/react-components-lab/src/WeatherForcast.css";
+const WeatherForecast = (props) => {
+  return (
+    <div className="weather">
+      <h2>{props.weather.day}</h2>
+      <img src={props.weather.img} alt={props.weather.altImg} />
+      <p>
+        <span></span>
+        {props.weather.condition}
+      </p>
+      <p>
+        <span></span>
+        {props.weather.time}
+      </p>
+    </div>
+  );
+};
+
+export default WeatherForecast;
